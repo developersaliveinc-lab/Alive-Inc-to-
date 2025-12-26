c9867a010f9ebbcf74c8f1622b063c38b77c067a
atul.alive <atul.aliveinc@gmail.com>
2025-12-26 18:00:57 +0530

    Fix images and update components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -24,7 +24,7 @@ const Footer = () => {
           <ul>
             <li><a href="/">Home</a></li>
             <li><a href="/about">About Us</a></li>
-            <li><a href="/products">Product Categories</a></li>
+            <li><a href="/products">Product </a></li>
             <li><a href="/contact">Contact Us</a></li>
           </ul>
         </div>
